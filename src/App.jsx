41c777283c1d2618092f8205b6836abf6bb98a5d
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2023-01-18 00:50:28 +0100

    completed division into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import logo from './logo.svg'
 import './App.css'
+import MainNav from './comps/MainNav'
+import MainSearch from './comps/MainSearch'
+import MainTracksFilter from './comps/MainTracksFilter'
+import Content from './comps/Content'
+import Sidebar from './comps/Sidebar'
+import PlayerBar from './comps/PlayerBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <main className="main">
+          <MainNav />
+          <div className="main__centerblock centerblock">
+            <MainSearch />
+            <h2 className="centerblock__h2">Треки</h2>
+            <MainTracksFilter />
+            <Content />
+          </div>
+          <Sidebar name="Sergey.Ivanov" />
+        </main>
+        <PlayerBar title="Ты та..." author="Баста" />
+        <footer className="footer"></footer>
+      </div>
     </div>
   )
 }
---
+++ src/comps/MainNav.jsx
@@ -0,0 +1,37 @@
+import logo from '../img/logo.png'
+
+function MainNav() {
+  return (
+    <nav className="main__nav nav">
+      <div className="nav__logo logo">
+        <img className="logo__image" src={logo} alt="logo"></img>
+      </div>
+      <div className="nav__burger burger">
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+      </div>
+      <div className="nav__menu menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Главное
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Мой плейлист
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Войти
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default MainNav
